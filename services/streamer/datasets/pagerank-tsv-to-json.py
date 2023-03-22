091dd02c9650e2ce9c80ec24ab98ff3196737cb7
Sergey Kulik <104143901+zxqfd555-pw@users.noreply.github.com>
2023-03-22 22:33:58 +0100

    save script for backfilling dataset (#3500)
    GitOrigin-RevId: 40f0ba74c447bed60610d1cfb04af4f63b9eae6c

--- services/streamer/datasets/pagerank-tsv-to-json.py
+++ services/streamer/datasets/pagerank-tsv-to-json.py
@@ -2,12 +2,14 @@ import json
 
 if __name__ == "__main__":
     is_first_row = True
-    with open("pagerank.tsv", "r") as f, open("pagerank.json", "w") as fw:
+    with open("soc-LiveJournal1.txt", "r") as f, open(
+        "pagerank-backfill.json", "w"
+    ) as fw:
         for row in f:
-            if is_first_row:
-                is_first_row = False
+            if row.startswith("#"):
                 continue
+
             tokens = row.strip().split("\t")
-            assert len(tokens) == 3
-            result = {"u": int(tokens[1]), "v": int(tokens[2])}
+            assert len(tokens) == 2
+            result = {"u": int(tokens[0]), "v": int(tokens[1])}
             fw.write(json.dumps(result) + "\n")
