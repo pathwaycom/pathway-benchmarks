410c76cc1a2d799a62d081d579f3f7ebafeb4db6
Mateusz Lewandowski <lewymati@users.noreply.github.com>
2023-03-28 12:02:47 +0200

    Flink run all + memory reports (#3480)
    * flink local run all
    
    * .
    
    GitOrigin-RevId: 124fd0a02082fe80edc07efbaaed720158c729f2

--- services/flink-pagerank/pagerank/src/main/scala/flink-pagerank-scala/App.scala
+++ services/flink-pagerank/pagerank/src/main/scala/flink-pagerank-scala/App.scala
@@ -20,7 +20,8 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.api.common.RuntimeExecutionMode
 import org.apache.flink.api.java.utils.ParameterTool
-
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
+import org.apache.flink.metrics.slf4j.Slf4jReporterFactory
 
 object App
 {
@@ -29,14 +30,19 @@ object App
         val parameters = ParameterTool.fromArgs(args)
         val pagerank_iterations: Int = parameters.getRequired("pagerank_iterations").toInt
         val inputFileName = parameters.getRequired("input")
+        val parallelism = parameters.getRequired("parallelism")
 
-        val settings = EnvironmentSettings
-        .newInstance()
-        // .inStreamingMode()
-        .inBatchMode()
-        .build()
-        
-        val tableEnv = TableEnvironment.create(settings)
+        val configuration = new Configuration;
+        configuration.setString("table.exec.resource.default-parallelism", s"${parallelism}")
+        configuration.setString("taskmanager.memory.managed.size", "10G")
+
+        configuration.setString("metrics.reporter.slf4j.factory.class", "org.apache.flink.metrics.slf4j.Slf4jReporterFactory")
+        configuration.setString("metrics.reporter.slf4j.interval", "5 SECONDS")
+
+        val env = StreamExecutionEnvironment.createLocalEnvironment(1, configuration)
+
+        // val tableEnv = TableEnvironment.create(settings)
+        val tableEnv = StreamTableEnvironment.create(env, EnvironmentSettings.inBatchMode)
         
         tableEnv.createTemporaryTable("SourceTable", TableDescriptor.forConnector("filesystem")
         .schema(Schema.newBuilder()
@@ -49,11 +55,13 @@ object App
 
         // // Create a sink table (using SQL DDL)
         tableEnv.executeSql("CREATE TEMPORARY TABLE SinkTable (`id_rank` INT, `rank` BIGINT) WITH ('connector' = 'blackhole');")
+        // tableEnv.executeSql("CREATE TEMPORARY TABLE SinkTable (`id_rank` INT, `rank` BIGINT) WITH ('connector' = 'filesystem', 'path'='file:////app/data/out.json', 'format'='json');")
+        // tableEnv.executeSql("CREATE TEMPORARY TABLE SinkTable (`id_rank` INT, `rank` BIGINT) WITH ('connector' = 'print');")
 
         val edges = tableEnv.from("SourceTable")
         val in_vertices = edges.groupBy($"v").select($"v".as("id"), 0.as("degree0"))
         val out_vertices = edges.groupBy($"u").select($"u".as("id"), $"v".count.as("degree"))
-        val degrees = in_vertices.leftOuterJoin(out_vertices.select($"id".as("id_out"), $"degree"), $"id" === $"id_out").select($"id", $"degree".ifNull(0).as("degree"))
+        val degrees = in_vertices.fullOuterJoin(out_vertices.select($"id".as("id_out"), $"degree"), $"id" === $"id_out").select($"id".ifNull($"id_out").as("id"), $"degree".ifNull(0).as("degree"))
         val base = out_vertices.select($"id").minus(in_vertices.select($"id")).select($"id", 0.as("flow"))  // minus works only in batch! Reimplementing below with join+filter
         // val base = out_vertices.select($"id").leftOuterJoin(in_vertices.select($"id".as("id_in")), $"id" === $"id_in").filter($"id_in".isNull).select($"id", 0.as("flow"))
         var ranks = degrees.select($"id".as("id_rank"), 6000.as("rank"))
