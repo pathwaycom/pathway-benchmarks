9a40f6a59a5619d8dc7e415ec290af36dac04366
Sergey Kulik <104143901+zxqfd555-pw@users.noreply.github.com>
2023-03-31 17:15:35 +0200

    better ux in benchmarks doall console script (#3566)
    GitOrigin-RevId: 1e13d7f5e51c72e29f7032147d2c04b0eda1413c

--- services/pathway-all/doall_batch.py
+++ services/pathway-all/doall_batch.py
@@ -1,30 +1,49 @@
+import argparse
 import os
 import subprocess
 
-CPUS = [1, 2, 4, 6, 8, 10, 12]
-PR_STEPS = [1, 2, 5, 10, 20]
-N_REPEATS = 5
-
-BASE_LAUNCH_COMMAND = "taskset --cpu-list {} python main.py --type pagerank --autocommit-frequency-ms 1000000000 --channel fs --input-filename ../streamer/datasets/pagerank-5m-backfill.json --pagerank-steps {}"  # noqa: E501
+BASE_LAUNCH_COMMAND = "taskset --cpu-list {} python main.py --type pagerank --autocommit-frequency-ms 1000000000 --channel fs --input-filename {} --pagerank-steps {}"  # noqa: E501
 
 
-def taskset_string(n_cpus):
-    cpu_ids = []
-    for i in range(n_cpus):
-        cpu_ids.append(str(i))
-    return ",".join(cpu_ids)
+def taskset_string(cpu_pool, n_cpus):
+    return ",".join(str(cpu_id) for cpu_id in cpu_pool[:n_cpus])
+
+
+def parse_args_list(raw_repr):
+    return [int(x) for x in raw_repr.strip().split(",")]
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Batch and backfilling experiments coordinator"
+    )
+    parser.add_argument("--dataset-path", type=str, required=True)
+    parser.add_argument("--available-core-ids", type=str, required=True)
+    parser.add_argument("--n-cores-to-test", type=str, required=True)
+    parser.add_argument("--n-steps-to-test", type=str, default="5")
+    parser.add_argument("--repeats", type=int, default=5)
+    args = parser.parse_args()
+
     os.environ["PATHWAY_YOLO_RARE_WAKEUPS"] = "1"
     os.environ["PATHWAY_PROFILE"] = "release"
-    for n_cpus in CPUS:
-        os.environ["PATHWAY_THREADS"] = str(n_cpus)
-        for n_steps in PR_STEPS:
-            for n_repeat in range(N_REPEATS):
-                command = BASE_LAUNCH_COMMAND.format(taskset_string(n_cpus), n_steps)
 
-                args = command.split()
+    available_core_ids = parse_args_list(args.available_core_ids)
+    n_cores_to_test = parse_args_list(args.n_cores_to_test)
+    n_steps_to_test = parse_args_list(args.n_steps_to_test)
+    n_repeats = args.repeats
+    dataset_path = args.dataset_path
+
+    for n_cpus in n_cores_to_test:
+        os.environ["PATHWAY_THREADS"] = str(n_cpus)
+        for n_steps in n_steps_to_test:
+            command = BASE_LAUNCH_COMMAND.format(
+                taskset_string(available_core_ids, n_cpus),
+                dataset_path,
+                n_steps,
+            )
+            args = command.split()
+
+            for n_repeat in range(n_repeats):
                 print("Executing...", args)
 
                 popen = subprocess.Popen(args, stdout=subprocess.PIPE)
